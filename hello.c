0e0934ddeeedc7adf4f1593ecdd1adacb00fca02
Roland-ho <1921099735@qq.com>
2026-05-31 00:11:06 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,5 +1,7 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 int main() {
-  printf("Hello,World!"我是罗兰今天开始第一天学习。\n");
-  return 0;
+    printf("Hello, World! ��ã����磡\n");
+    system("pause");
+    return 0;
 }
